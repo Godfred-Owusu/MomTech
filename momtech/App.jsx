79fc52911063b66f7410809bda72e63bc76f2a2a
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-09-14 19:51:51 +0000

    Setup the server boilerplate. Also updated the frontend boilerplate with a couple directories. Waiting for midnight to download all the neccessary dependencies and start working.

--- momtech/App.jsx
+++ momtech/App.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mom Tech is a go gentlemen!</Text>
       <StatusBar style="auto" />
     </View>
   );
