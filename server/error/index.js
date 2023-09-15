5acc1b3913c1f624fc48d4052139151da64aa09a
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-09-15 04:45:58 +0000

    Included my custom error class for handling likely known errors.

---
+++ server/error/index.js
@@ -0,0 +1,12 @@
+const CustomError = require('./CustomError')
+const BadRequestError = require('./BadRequestError')
+const NotFoundError = require('./NotFoundError')
+const UnAuthorized = require('./UnAuthorizedError')
+
+
+module.exports = {
+     CustomError,
+     BadRequestError,
+     NotFoundError,
+     UnAuthorized
+}
